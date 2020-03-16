d4a71e3ffcf0fdb29b210cc853e110fb99558241
keiralien <jen.murphy8200@gmail.com>
2020-03-16 18:44:20 -0500

    authentication studio complete

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/LoginFormDTO.java
@@ -0,0 +1,33 @@
+package org.launchcode.javawebdevtechjobsauthentication.models.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class LoginFormDTO {
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 20, message = "Username must between 3 and 20 characters.")
+    private String username;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters.")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword () {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
